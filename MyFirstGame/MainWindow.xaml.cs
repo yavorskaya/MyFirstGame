dde98544c1f964741dcec23e0efae84297b96361
taffytf <taffytf@gmail.com>
2022-07-19 20:19:54 +0300

    style dictionary + new order of games. Animals are visible for 20 seconds to memories. Then hidden, and open only when mached.

--- MyFirstGame/MainWindow.xaml.cs
+++ MyFirstGame/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MyFirstGame
 {
@@ -20,10 +22,42 @@ namespace MyFirstGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        int secondsElapsed;
+        int timeToMemorise = 20000;
+        int matchFound = 8;
+
         public MainWindow()
         {
             InitializeComponent();
-            SetUpGame();
+            
+
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            dispatcherTimer.Tick += Timer_Tick;
+            
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeToMemorise == 0)
+            {
+                secondsElapsed += 1000;
+                txtblockTime.Text = (secondsElapsed / 1000).ToString("0 s");
+                if (matchFound == 8)
+                {
+                    dispatcherTimer.Stop();
+                    txtblockTime.Text = txtblockTime.Text;
+                    btnPlay.Visibility = Visibility.Visible;
+                    btnPlay.Content = "Play again";
+                }
+            }
+            else
+            {
+                timeToMemorise -= 1000;
+                txtblockTime.Text = (timeToMemorise / 1000).ToString("0 s");
+                if (timeToMemorise == 0)
+                    HideAnimals();
+            }
         }
 
         private void SetUpGame()
@@ -42,12 +76,71 @@ namespace MyFirstGame
             };
 
             Random random = new Random();
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            foreach(Grid grid in mainGrid.Children.OfType<Grid>())
             {
+                TextBlock tb = grid.Children[0] as TextBlock; 
                 int index = random.Next(animalEmoji.Count);
                 string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
+                tb.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+                tb.Visibility = Visibility.Visible;
+            }
+            dispatcherTimer.Start();
+            matchFound = secondsElapsed = 0;
+            timeToMemorise = 20000;
+            btnPlay.Visibility=Visibility.Hidden;
+            mainGrid.IsHitTestVisible = findingMatch = false;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void HideAnimals()
+        {
+            mainGrid.IsHitTestVisible = true;
+            foreach (Grid grid in mainGrid.Children.OfType<Grid>())
+            {
+                TextBlock tb = grid.Children[0] as TextBlock;
+                tb.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            TextBlock tb = grid?.Children[0] as TextBlock;
+            if (tb == null)
+                return;
+            tb.Visibility = Visibility.Visible;
+            if (!findingMatch)
+            {
+                lastTextBlockClicked = tb;
+                findingMatch = true;
+            }
+            else if (tb.Text == lastTextBlockClicked.Text)
+            {
+                findingMatch = false;
+                ++matchFound;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (matchFound == 8)
+                SetUpGame();
+        }
+
+        private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            TextBlock tb = grid?.Children[0] as TextBlock;
+            if (tb == null)
+                return;
+            tb.Visibility = Visibility.Visible;
+            if (findingMatch && tb.Text != lastTextBlockClicked.Text)
+            {
+                lastTextBlockClicked.Visibility = tb.Visibility = Visibility.Hidden;
+                findingMatch = false;
             }
         }
     }
